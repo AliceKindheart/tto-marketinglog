d9c3095529aecd7b09295dc725bc6d4b09be025e
Alice Kindheart <moonstonecowgirl@gmail.com>
2016-11-21 14:44:30 -0500

    fixing

---
+++ app/routes/contacts.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+* Module dependencies.
+*/
+var users = require('../../app/controllers/users'),
+contacts = require('../../app/controllers/contacts');
+//companies = require('../../app/controllers/companies');
+
+module.exports = function(app) {
+// Company Routes
+app.route('/contacts')
+    .get(contacts.all)
+    .post(users.requiresLogin, contacts.create);
+//app.route('/companies/create')
+//	.get(companies.all);
+app.route('/contacts/:id')
+    .get(contacts.show)
+    .put(users.requiresLogin, contacts.update)
+    .delete(users.requiresLogin, contacts.destroy);
+
+
+// Finish with setting up the id param
+// Note: the companies.company function will be called everytime then it will call the next function.
+app.param('id', contacts.contact);
+};
+
